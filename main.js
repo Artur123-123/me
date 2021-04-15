cd297e8a50c058ae3bbdd5e0769324d2424f83a7
Artur-123-123 <arturolejnik462@gmail.com>
2021-04-15 21:26:50 +0200

    me

---
+++ main.js
@@ -0,0 +1,7 @@
+const div = document.createElement('div')
+const p = document.createElement('p')
+const text = document.createTextNode('Jaro')
+
+app.appendChild(div)
+div.appendChild(p)
+p.appendChild(text)
